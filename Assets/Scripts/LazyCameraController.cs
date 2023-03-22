55d0eb6ab485930b5fc4b6f64aab13448c3c0ab1
RhysNortonMK <rhys.norton@mightykingdom.com>
2023-03-23 00:01:16 +1030

    Added zoom bounds and restricted pan area to unlocked planets

--- Assets/Scripts/LazyCameraController.cs
+++ Assets/Scripts/LazyCameraController.cs
@@ -5,6 +5,9 @@ public class LazyCameraController : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [SerializeField] private float cameraHeightMovementSpeed;
     [SerializeField] private float cameraRotationSpeed;
+    [SerializeField] private float minZoom = 10f;
+    [SerializeField] private float maxZoom = 50f;
+    public float FurthestPlanetMagnitude { get; set; }
 
     private Vector3 movementInputVector;
     private float cameraHeightInput;
@@ -15,13 +18,19 @@ public class LazyCameraController : MonoBehaviour
         UpdateInputVector();
         UpdateCameraHeight();
         //UpdateCameraRotationInput();
-        transform.position += transform.TransformDirection((movementInputVector * movementSpeed) * Time.deltaTime);
+        transform.position += transform.TransformDirection((movementInputVector * movementSpeed) * Time.deltaTime);        
         transform.position += new Vector3(0.0f, cameraHeightInput * cameraHeightMovementSpeed * Time.deltaTime, 0.0f);
+
+        Vector2 positionXZ = new Vector3(transform.position.x, transform.position.z);
+        if (positionXZ.magnitude > FurthestPlanetMagnitude) positionXZ = Vector3.Normalize(positionXZ) * FurthestPlanetMagnitude;
+        float positionY = Mathf.Clamp(transform.position.y, minZoom, maxZoom);
+
+        transform.position = new Vector3(positionXZ.x, positionY, positionXZ.y);
         //transform.RotateAround(Vector3.up, cameraRotationInput * cameraRotationSpeed * Time.deltaTime);
     }
 
     void UpdateInputVector()
-    {
+    {        
         movementInputVector = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
     }
 
